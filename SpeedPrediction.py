0f01bf0facc8ba2e179949c606ccbb33e589247a
Lihakk <58601789+Lihakk@users.noreply.github.com>
2024-08-17 21:23:33 +0300

    Update SpeedPrediction.py

--- SpeedPrediction.py
+++ SpeedPrediction.py
@@ -5,14 +5,11 @@ import soundfile as sf
 from EnvAPI import get_API
 from transcription import transcribeAudio
 from TextToSpeech import Speech
-def FindSpeed():
-    Speech(transcribeAudio(),get_API(),"test_video_AI.mp3")
-    OrgFile = sf.SoundFile("test_video_AI.mp3")
+def FindSpeed(filename,TTSfilename):
+    Speech(transcribeAudio(),get_API(),TTSfilename)
+    OrgFile = sf.SoundFile(TTSfilename)
     OrgSeconds = OrgFile.frames/OrgFile.samplerate
-    VideoFileClip("test_video.mp4").audio.write_audiofile("test_video.mp3")
+    VideoFileClip(filename).audio.write_audiofile("test_video.mp3")
     ActualFile = sf.SoundFile("test_video.mp3")
     ActualSeconds = ActualFile.frames/ActualFile.samplerate
-    print(OrgSeconds)
-    print(ActualSeconds)
     return OrgSeconds / ActualSeconds
-print(FindSpeed())
